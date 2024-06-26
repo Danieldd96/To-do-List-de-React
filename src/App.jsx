ec4c4a2826d326ba3b3dc4985345aeb5fb041400
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-06-26 13:45:29 -0600

    Correcion de el contador mas creacion de animaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import './App.css'
 import Header from './componentes/Header'
 import Contenedor from './componentes/Contenedor'
-import Completadas from './componentes/Completadas'
+
 import Tareas from './componentes/Tareas'
-import { useRef,useState } from 'react'
+import { useRef } from 'react'
 
 function App() {
   const inputRef = useRef();{/*Este input ref me ayudara para obtener el valor del input*/}
 
-  const [contador, setContador] = useState(0);{/*Este useState lo utilizare para crear el contador de tareas completadas*/}
+
 
 
   return (
     <>
       <div>
         <Header/>{/*Este header contien lo que es el titulo y lo que son las redes sociales con sus respectivos inputs*/} 
-        <Completadas contador={contador} setContador={setContador}></Completadas>{/*Aqui completadas es la parte que muestra el contador de tareas que completamos al marcar el checkbox  */}
         <Contenedor ref1={inputRef}></Contenedor>{/*Este contenedor tiene el input y el boton que usaremos para guardar las tareas en el fetch*/}
-        <Tareas/> {/*En tareas es donde se mostraran las respectivas tareas que guardamos en el fetch*/}
+        <Tareas /> {/*En tareas es donde se mostraran las respectivas tareas que guardamos en el fetch*/}
       </div>
     </>
   )
--- src/componentes/Tareas.jsx
+++ src/componentes/Tareas.jsx
@@ -3,19 +3,29 @@ import Get from './Get';
 import deleteData from './Delete';
 import actualizarTarea from './Put';
 
-const Tareas = () => {                              {/*Con esta funcion Tareas se obtendran las tareas guardadas previamente para mostrarlas luego en pantalla*/}
+const Tareas = () => {         
+    const [contador,setContador]=useState(0);                    {/*Con esta funcion Tareas se obtendran las tareas guardadas previamente para mostrarlas luego en pantalla*/}
     const [lista, setLista] = useState([]);         {/*Este use state sera para que lista contenga las tareas en un array*/}
     useEffect(() => {
         const ListarTareas = async () => {          {/*ListarTareas hara que lista tenga las tareas obteniendolas del metodo get*/}
             const Lista = await Get();
             setLista(Lista);                        {/*Y con el setLista le pondremos el estado de la Lista previamente mencionada*/}
         };
+        const tareasHechas = lista.filter(tarea=>tarea.estado===true).length
+        setContador(tareasHechas)
         ListarTareas();                              
     }, [lista]);                                    {/*Con el useEffect haremos que la funcion se ejecute al menos una vez o cuando la lista cambie*/}
-    
+
     const handleCheckboxClick = async (task) => {   {/*Con esta funcion cambio el estado de las tareas*/}
         await actualizarTarea(task);
-        
+        const change = async () => {      
+            let conteo = 0;                {/*Este let conteo sera el que recibira el numero final que mostrare en el contador*/}
+            setContador(conteo);{/*Con este setContador que recibimos de la funcion como parametro hacemos que el parametro contador contenga ahora el numero de conteo*/}
+            
+        };
+
+        change();  {/*Con esto llamamos la funcion*/}
+
     };
 
     const handleDeleteClick = async (id) => {      {/*con esta funcion elimino la tarea*/}
@@ -24,6 +34,10 @@ const Tareas = () => {                              {/*Con esta funcion Tareas s
 
     return (
         <div className="tasks">
+            {lista.length===0?undefined:<div className="completadas">
+                <p className="tarCom">Tareas completadas</p>
+                <p id="contador">{contador}</p>    {/*En esta etiqueta <p> su contenido sera contador que ya tendria el numero nuevo que se mostrara en pantalla*/}
+            </div>}
             {/*Con esta parte creo el mensaje de no hay tareas viendo si lista.lenght es igual a o cree el mensaje y si detecta que hay tareas no muestre ningun mensaje*/}
             {lista.length === 0 ? <div className='noTasks'><img className='imgChibi' src="src/img/firefly.gif"/><h2>No hay tareas ingresa una con el input de arriba</h2></div>: undefined}
             {/*Esta parte sera el como se mostrara en pantalla cada una de las tareas aqui lista la usaremos con un map y 
